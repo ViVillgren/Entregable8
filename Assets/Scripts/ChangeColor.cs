878f356b00b31240b591f2c925b86dbdf2c7f97d
Victor Navarro Villgren <victorvillgren@hotmail.com>
2022-02-25 18:36:23 +0100

    up

--- Assets/Scripts/ChangeColor.cs
+++ Assets/Scripts/ChangeColor.cs
@@ -5,7 +5,7 @@ using UnityEngine.UI;
 
 public class ChangeColor : MonoBehaviour
 {
-
+    //Variables para coger el cubo, cambiar el render del cubo y los canaales de color para hacerlo aleatorio.
     [SerializeField] private GameObject cube;
     private Renderer cubeRenderer;
     private Color newCubeColor;
@@ -13,12 +13,15 @@ public class ChangeColor : MonoBehaviour
 
     void Start()
     {
+        //funcion cada vez que clickemos el boton el gameObject cambiara a un color aleatorio 
         cubeRenderer = cube.GetComponent<Renderer>();
         gameObject.GetComponent<Button>().onClick.AddListener(ChangeCubeColor);
     }
 
     private void ChangeCubeColor()
     {
+
+        //canales de colores que se aplicara uno aleatoriamente cada vez que clickemos el boton
         randomChannelOne = Random.Range(0f, 1f);
         randomChannelTwo = Random.Range(0f, 1f);
         randomChannelThree = Random.Range(0f, 1f);
--- Assets/Scripts/RotateSlider.cs
+++ Assets/Scripts/RotateSlider.cs
@@ -6,18 +6,20 @@ using UnityEngine.UI;
 public class RotateSlider : MonoBehaviour
 {
 
+    //Variables para coger el Slider y añadir un maximo al que queramos rotar el objecto, en este caso en Y
+
     public Slider mySlider;
     public float yLimit = 180f;
 
-
-    // Start is called before the first frame update
     void Start()
     {
+        //Al mover el Slider rotara el objeto
         mySlider.onValueChanged.AddListener(delegate { RotateMe(); });
     }
 
     public void RotateMe()
     {
+        //Funcion para que el objecto rote segun el valor del Slider
         transform.localEulerAngles = new Vector3(0, mySlider.value * yLimit, 0);
     }
 }
